5975671b6da96c0f4ec25365800932bda61919a8
Toma <100084874+Tomajashi@users.noreply.github.com>
2025-07-01 09:57:33 +0200

    .

--- src/main/java/de/gts/redrail/game/component/PlayComponentsStore.java
+++ src/main/java/de/gts/redrail/game/component/PlayComponentsStore.java
@@ -41,7 +41,6 @@ public class PlayComponentsStore {
         player.getResourceRack().setDbCoin(dbCoin - (player.getRails().size()*NEW_RAIL)); 
 
         return new ActionResult(true, BOUGHT_NEW_PLAY_COMPONENT);
-        // The method returns an ActionResult indicating success and the message for a new rail purchase
     }
 
     public ActionResult upgradeRail(Player player, String railUid) {
