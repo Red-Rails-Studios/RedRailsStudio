4f1985024d42f58c1fb82ed498fb3e50059dffae
Toma <100084874+Tomajashi@users.noreply.github.com>
2025-07-01 15:32:33 +0200

    marcoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo

--- src/main/java/de/gts/redrail/game/service/MapGenerationService.java
+++ src/main/java/de/gts/redrail/game/service/MapGenerationService.java
@@ -14,12 +14,39 @@ public class MapGenerationService {
     Map map = new Map();
     Random random = new Random();
 
+   private static final String[] GERMAN_CITIES = {
+    "Aach", "Aachen", "Aalen", "Abenberg", "Abensberg", "Achern", "Achim", "Adelsheim", "Adorf/Vogtl.", 
+    "Ahaus", "Ahlen", "Ahrensburg", "Aichach", "Aken", "Albstadt", "Alfeld", "Allendorf", "Allersberg",
+    "Alsdorf", "Alsfeld", "Altena", "Altenberg", "Altenburg", "Altentreptow", "Altlandsberg", 
+    "Altdorf bei Nürnberg", "Altensteig", "Altötting", "Alzenau", "Amberg", "Amorbach", "Andernach", 
+    "Angermünde", "Anklam", "Annaberg-Buchholz", "Annweiler", "Ansbach", "Apolda", "Arnsberg", "Arnstadt",
+    "Aschaffenburg", "Aschersleben", "Asperg", "Attendorn", "Aub", "Aue-Bad Schlema", "Auerbach", 
+    "Augsburg", "Augustusburg", "Aurich", "Babenhausen", "Backnang", "Bad Aibling", "Bad Berleburg",
+    "Bad Dürkheim", "Bad Essen", "Bad Fallingbostel", "Bad Gandersheim", "Bad Harzburg", 
+    "Bad Homburg vor der Höhe", "Bad Honnef", "Bad Kissingen", "Bad Kreuznach", "Bad Langensalza",
+    "Bad Lauchstädt", "Bad Liebenwerda", "Bad Mergentheim", "Bad Münder", "Bad Nauheim", 
+    "Bad Nenndorf", "Bad Neustadt an der Saale", "Bad Oldesloe", "Bad Pyrmont", "Bad Rappenau",
+    "Bad Reichenhall", "Bad Salzdetfurth", "Bad Salzuflen", "Bad Säckingen", "Bad Saulgau", 
+    "Bad Schandau", "Bad Schwartau", "Bad Segeberg", "Bad Soden am Taunus", "Bad Soden-Salmünster", 
+    "Bad Staffelstein", "Bad Tölz", "Bad Vilbel", "Bad Waldsee", "Bad Wildbad", "Bad Wildungen",
+    
+};
+
+
     public Location generateRandomLocation() {
+        int yesorno = random.nextInt(2);
+
+        if(yesorno == 0)
+        {
+            return null;
+        }
+
         LocationEnum[] locationTypes = LocationEnum.values();
         int randomIndex = random.nextInt(locationTypes.length);
         LocationEnum randomType = locationTypes[randomIndex];
 
         Integer customers = 0;
+        
         switch (randomType) {
             case VILLAGE:
                 customers = random.nextInt(100) + 50; // 50 to 150 customers
@@ -35,7 +62,13 @@ public class MapGenerationService {
                 break;
         }
         
+        String name = getRandomGermanCityName();
 
         return new Location(randomType, name, customers, null);
+    }
+
+    private String getRandomGermanCityName() {
+        int idx = random.nextInt(GERMAN_CITIES.length);
+        return GERMAN_CITIES[idx];
     }
 }
