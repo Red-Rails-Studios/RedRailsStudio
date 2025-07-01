d4c8ff26c17dfc8cf6bf973fd6cdd5ec10d46928
Petarbyte <pgeorgiev64@yahoo.com>
2025-07-01 14:38:58 +0200

    Hauptseite, Username feld und Start Button der zu weiteren Buttons template fuehrt

--- rrs_frontend/src/app/pages/homepage/homepage.component.ts
+++ rrs_frontend/src/app/pages/homepage/homepage.component.ts
@@ -1,11 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-homepage',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './homepage.component.html',
-  styleUrl: './homepage.component.scss'
+  styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent {
+  sessionName: any;
+  playerName = '';
+  inputValue: any = '';
+  showOptions: any;
 
+  startMultiplayer() {
+    if (this.sessionName) {
+      console.log('Creating multiplayer session:', this.sessionName);
+    }
+  }
+
+  joinMultiplayer() {
+    if (this.playerName) {
+      console.log('Joining multiplayer session:', this.playerName);
+    }
+  }
+
+  startSingleplayer() {
+    console.log('Singleplayer session started');
+  }
+
+  endSingleplayer() {
+    console.log('Singleplayer session ended');
+  }
 }
