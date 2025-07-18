0d320c728c1b9fc83b7b6232e6a3ae2d92cc7079
Tomajashi <tomajashi5@gmail.com>
2025-07-18 12:12:18 +0200

    ///////

--- src/main/java/de/gts/redrail/WebConfig.java
+++ src/main/java/de/gts/redrail/WebConfig.java
@@ -9,10 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*") 
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
-                .allowedHeaders("*")
+                .allowedOriginPatterns("http://localhost:4200")
                 .allowCredentials(true);
     }
 }
-    
