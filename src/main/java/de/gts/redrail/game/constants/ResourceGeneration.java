a48005c9e434069a908fb48110b1e5ded0b1dbcd
Tomajashi <tomajashi5@gmail.com>
2025-10-22 18:35:54 +0200

    .

--- src/main/java/de/gts/redrail/game/constants/ResourceGeneration.java
+++ src/main/java/de/gts/redrail/game/constants/ResourceGeneration.java
@@ -2,7 +2,8 @@ package de.gts.redrail.game.constants;
 public interface ResourceGeneration {
 
     Integer STATION_RESOURCE_GENERATION_FACTOR = 2;
-    Integer TRAIN_RESOURCE_GENERATION_FACTOR = 11;
+    Integer TRAIN_RESOURCE_GENERATION_FACTOR = 10;
     
-    Long RESOURCE_GENERATION_INTERVAL_IN_SECONDS = 5L;
+    // Generation interval in seconds; set to 1s for more frequent updates
+    Long RESOURCE_GENERATION_INTERVAL_IN_SECONDS = 1L;
 }
